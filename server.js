220458df4026a0bf1b00dddc5a6d59b9b2b0aa98
Your Name <youremail@example.com>
2026-01-20 22:55:50 +0530

    final one

--- server.js
+++ server.js
@@ -221,7 +221,7 @@ app.prepare().then(() => {
 
     });
 
-    const PORT = 8080;
+    const PORT = process.env.PORT || 3000;
 
     server.listen(PORT, () => {
         console.log("🚀 Server running on port", PORT);
